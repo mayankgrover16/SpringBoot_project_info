b647f3551ed027ffa2447fbf2a4a2175b5fa3423
mayankgrover16 <91666303+mayankgrover16@users.noreply.github.com>
2025-03-15 14:36:43 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void print(){
         System.out.println("Hello Word");
     }
+    public static void PrintCast(){
+     System.out.println("Grover");
+    }
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            
             System.out.println("i = " + i);
         }
     }
