23028c8aa0fa752484fd46039e4a55a4a2c3c5e6
mayankgrover16 <91666303+mayankgrover16@users.noreply.github.com>
2025-03-15 15:34:23 +0530

    Update SingletonPattern.java

--- src/SingletonPattern.java
+++ src/SingletonPattern.java
@@ -1,15 +1,7 @@
 public class SingletonPattern {
-    public static void print(){
-        System.out.println("Hello Word");
-    }
+    
 
-    public static void PrintName(){
-     System.out.println("Mayank");
-    }
-
-    public static void PrintHeight(){
-     System.out.println("6 and 5");
-    }
+    
     public static void main(String[] args) {
         System.out.println("Helloworld");
     }
